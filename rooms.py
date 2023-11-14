4fba9dc54e66d13b901ea67195411f5654ef0ee7
Harry Sweet <hsweet52@gmail.com>
2023-11-14 13:45:21 -0500

    resolved module dependencies, updated phone

--- rooms.py
+++ rooms.py
@@ -1,17 +1,16 @@
 
-#from play import *
-from play import inventory,print_room_description, navigate
+import play
+#from play import inventory,print_room_description, navigate
 from time import sleep
 
 #########################  Rooms ########################
-
 
 def road():
   this_room = 'road'
   exits = {'e': entry}
   objects_in_room = []  # No objects in this room
-  print_room_description(this_room, objects_in_room)
-  key, destination = navigate(exits, this_room, objects_in_room)
+  play.print_room_description(this_room, objects_in_room)
+  key, destination = play.navigate(exits, this_room, objects_in_room)
   destination()
 
 
@@ -19,9 +18,9 @@ def kitchen():
   this_room = 'kitchen'
   exits = {'e': finish, 's': entry}
   objects_in_room = ['book']  # Objects available in this room
-  objects_in_room = [obj for obj in objects_in_room if obj not in inventory]
-  print_room_description(this_room, objects_in_room)
-  key, destination = navigate(exits, this_room, objects_in_room)
+  objects_in_room = [obj for obj in objects_in_room if obj not in play.inventory]
+  play.print_room_description(this_room, objects_in_room)
+  key, destination = play.navigate(exits, this_room, objects_in_room)
   destination()
 
 
@@ -29,9 +28,9 @@ def entry():
   this_room = "entry"
   exits = {'n': kitchen, 's': closet}
   objects_in_room = ['key']  # Objects available in this room
-  objects_in_room = [obj for obj in objects_in_room if obj not in inventory]
-  print_room_description(this_room, objects_in_room)
-  key, destination = navigate(exits, this_room, objects_in_room)
+  objects_in_room = [obj for obj in objects_in_room if obj not in play.inventory]
+  play.print_room_description(this_room, objects_in_room)
+  key, destination = play.navigate(exits, this_room, objects_in_room)
   destination()
 
 
@@ -39,9 +38,9 @@ def closet():
   this_room = 'closet'
   exits = {'n': entry}
   objects_in_room = ['door']
-  objects_in_room = [obj for obj in objects_in_room if obj not in inventory]
-  print_room_description(this_room, objects_in_room)
-  key, destination = navigate(exits, this_room, objects_in_room)
+  objects_in_room = [obj for obj in objects_in_room if obj not in play.inventory]
+  play.print_room_description(this_room, objects_in_room)
+  key, destination = play.navigate(exits, this_room, objects_in_room)
   destination()
 
 def secret_room():
@@ -49,13 +48,12 @@ def secret_room():
   this_room = 'secret_room'
   exits = {'n': kitchen}
   objects_in_room = []
-  print_room_description(this_room, objects_in_room)
-  key, destination = navigate(exits, this_room, objects_in_room)
+  play.print_room_description(this_room, objects_in_room)
+  key, destination = play.navigate(exits, this_room, objects_in_room)
   destination()
 
 
 def finish():
-  #print(dialog['finish'])  # in script figure it out later.
   sleep(2)
   print('''
 ### #           ###       #
